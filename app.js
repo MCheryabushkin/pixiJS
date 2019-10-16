58c0ee24dc600ebc7c0700a6411157598d9fc044
Maksim Cheryabushkin <ch.maks@list.ru>
2019-10-16 22:37:04 +0300

    Lesson 6. Sikvel

--- app.js
+++ app.js
@@ -14,14 +14,21 @@ graphic.x = app.renderer.width / 2;
 graphic.y = app.renderer.height / 2;
 app.stage.addChild(graphic);
 
-graphic.lineStyle(5, 0x00ff00);
+let radius = 50;
+
 graphic.beginFill(0xff0000);
-// graphic.drawCircle(0, 0, 100);
-graphic.drawRect(0, 0, 100, 200);
+graphic.arc(0, 0, radius, 0, Math.PI * 2);
 graphic.endFill();
 
 app.ticker.add(animate);
 
+let delta = 0;
 function animate() {
-    
+    delta += 0.04;
+    radius = 50 + Math.sin(delta) * 25;
+
+    graphic.clear();
+    graphic.beginFill(0xff0000);
+    graphic.arc(0, 0, radius, 0, Math.PI * 2);
+    graphic.endFill();
 }
