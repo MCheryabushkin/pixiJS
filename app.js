e0a4406a6452215fe6617df6b254a4dcd0d0a719
Maksim Cheryabushkin <ch.maks@list.ru>
2019-10-16 22:31:02 +0300

    Lesson 6. Graphic API

--- app.js
+++ app.js
@@ -9,43 +9,19 @@ const app = new PIXI.Application({
     height: _h
 });
 
-console.log(PIXI.utils.TextureCache);
+let graphic = new PIXI.Graphics();
+graphic.x = app.renderer.width / 2;
+graphic.y = app.renderer.height / 2;
+app.stage.addChild(graphic);
 
-let loader = PIXI.Loader.shared;
-loader.onComplete.add(handleLoaderComplete);
-loader.onLoad.add(handleLoadAsset);
-loader.onError.add(handleLoadError);
-loader.onProgress.add(handleLoadProgress);
+graphic.lineStyle(5, 0x00ff00);
+graphic.beginFill(0xff0000);
+// graphic.drawCircle(0, 0, 100);
+graphic.drawRect(0, 0, 100, 200);
+graphic.endFill();
 
-loader.add("zombie", "zombie.png")
-    .on("progress", handleLoadProgress)
-    .on("load", handleLoadAsset)
-    .on("error", handleLoadError)
-    .load();
-
-let img;
-
-function handleLoadProgress(loader, resource) {
-    console.log(loader.progress + "% loaded");
-}
-function handleLoadAsset(loader,resource) {
-    console.log("asset loaded " + resource.name);
-}
-function handleLoadError() {
-    console.log("load error");
-}
-function handleLoaderComplete() {
-    let texture = loader.resources.zombie.texture;
-    img = new PIXI.Sprite(texture);
-    img.anchor.x = 0.5;
-    img.anchor.y = 0.5;
-    app.stage.addChild(img);
-
-    app.ticker.add(animate);
-}
+app.ticker.add(animate);
 
 function animate() {
-    img .x = app.renderer.screen.width / 2;
-    img.y = app.renderer.screen.height / 2;
-    img.rotation += 0.04;
+    
 }
